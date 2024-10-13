9da5c87929e165022188da9a33d940beff97dd07
PeteOyable <julien.boulevart@gmail.com>
2024-10-13 14:50:34 -0400

    update linting for few files

--- nvim/.config/nvim/lua/plugins/feline.lua
+++ nvim/.config/nvim/lua/plugins/feline.lua
@@ -1,9 +1,9 @@
 return {
-	"freddiehaddad/feline.nvim",
-	lazy = false,
-	config = function()
-		require("feline").setup({
-			components = require("catppuccin.groups.integrations.feline").get(),
-		})
-	end,
+  "freddiehaddad/feline.nvim",
+  lazy = false,
+  config = function()
+    require("feline").setup({
+      components = require("catppuccin.groups.integrations.feline").get(),
+    })
+  end,
 }
