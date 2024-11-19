a0a7c5cf237cf71ab2905258ad770273af8899e3
PeteOyable <julien.boulevart@gmail.com>
2024-11-19 11:14:59 -0500

    rework formatting pluging

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -1,7 +1,30 @@
 return {
   "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require('conform').setup({
+      notify_on_error = false,
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        svelte = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        liquid = { "prettier" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'fallback'
+      }
+    })
+  end,
   keys = {
     {
       "<leader>f",
@@ -14,39 +37,5 @@ return {
       mode = "",
       desc = "[F]ormat file",
     },
-  },
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = true, cpp = true }
-      local lsp_format_opt
-      if disable_filetypes[vim.bo[bufnr].filetype] then
-        lsp_format_opt = "never"
-      else
-        lsp_format_opt = "fallback"
-      end
-      return {
-        timeout_ms = 500,
-        lsp_format = lsp_format_opt,
-      }
-    end,
-  },
-  formatters_by_ft = {
-    javascript = { "prettier" },
-    typescript = { "prettier" },
-    javascriptreact = { "prettier" },
-    typescriptreact = { "prettier" },
-    svelte = { "prettier" },
-    css = { "prettier" },
-    html = { "prettier" },
-    json = { "prettier" },
-    yaml = { "prettier" },
-    markdown = { "prettier" },
-    graphql = { "prettier" },
-    liquid = { "prettier" },
-    lua = { "stylua" },
   },
 }
