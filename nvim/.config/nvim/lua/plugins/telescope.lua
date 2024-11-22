6de649549b5621c4d9138c1f3e4fb4c5ee1f93aa
PeteOyable <julien.boulevart@gmail.com>
2024-11-22 10:01:03 -0500

    add todo comments in telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,114 +1,115 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  event = "VimEnter",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
+	"nvim-telescope/telescope.nvim",
+	event = "VimEnter",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
 
-      -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
-      build = "make",
+			-- `build` is used to run some command when the plugin is installed/updated.
+			-- This is only run then, not every time Neovim starts up.
+			build = "make",
 
-      -- `cond` is a condition used to determine whether this plugin should be
-      -- installed and loaded.
-      cond = function()
-        return vim.fn.executable('make') == 1
-      end
-    },
-    "nvim-tree/nvim-web-devicons",
-    "nvim-telescope/telescope-file-browser.nvim",
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+			-- `cond` is a condition used to determine whether this plugin should be
+			-- installed and loaded.
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		"nvim-tree/nvim-web-devicons",
+		"nvim-telescope/telescope-file-browser.nvim",
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-    telescope.setup({
-      file_ignore_patterns = { "node_modules", ".git" },
-      pickers = {
-        oldfiles = {
-          only_cwd = true,
-        },
-        find_files = {
-          hidden = true,
-          no_ignore = true,
-        },
-      },
-      defaults = {
-        initial_mode = "normal",
-        path_display = { "truncate" },
-        sorting_strategy = "ascending",
-        selection_strategy = "closest",
-        set_env = { ["COLORTERM"] = "truecolor" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next,     -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            ["<C-d>"] = actions.delete_buffer,
-          },
-          n = {
-            ["<C-d>"] = actions.delete_buffer,
-          },
-        },
-        color_devicons = true,
-      },
-      extensions = {
-        file_browser = {
-          hidden = true,
-          no_ignore = true,
-          hijack_netrw = true,
-          display_stat = {
-            mode = false,
-            size = false,
-            date = true,
-          },
-          grouped = true,
-          prompt_path = true,
-        },
-      },
-    })
+		telescope.setup({
+			file_ignore_patterns = { "node_modules", ".git" },
+			pickers = {
+				oldfiles = {
+					only_cwd = true,
+				},
+				find_files = {
+					hidden = true,
+					no_ignore = true,
+				},
+			},
+			defaults = {
+				initial_mode = "normal",
+				path_display = { "truncate" },
+				sorting_strategy = "ascending",
+				selection_strategy = "closest",
+				set_env = { ["COLORTERM"] = "truecolor" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-d>"] = actions.delete_buffer,
+					},
+					n = {
+						["<C-d>"] = actions.delete_buffer,
+					},
+				},
+				color_devicons = true,
+			},
+			extensions = {
+				file_browser = {
+					hidden = true,
+					no_ignore = true,
+					hijack_netrw = true,
+					display_stat = {
+						mode = false,
+						size = false,
+						date = true,
+					},
+					grouped = true,
+					prompt_path = true,
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
-    telescope.load_extension("file_browser")
-    telescope.load_extension("ui-select")
-    telescope.load_extension("neoclip")
+		telescope.load_extension("fzf")
+		telescope.load_extension("file_browser")
+		telescope.load_extension("ui-select")
+		telescope.load_extension("neoclip")
 
-    -- set keymaps
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
-    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
-    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
-    vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
-    vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
-    vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
-    vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-    vim.keymap.set(
-      "n",
-      "<leader>fe",
-      "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
-      { desc = "[F]ile [E]xplorer" }
-    )
-    vim.keymap.set("n", "<leader>fn", "<cmd>Telescope neoclip<CR>", { desc = "[F]ind in [N]eoclip" })
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set("n", "<leader>/", function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-        previewer = false,
-      }))
-    end, { desc = "[/] Fuzzily search in current buffer" })
+		-- set keymaps
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp" })
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "[F]ind [K]eymaps" })
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+		vim.keymap.set("n", "<leader>fs", builtin.builtin, { desc = "[F]ind [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "[F]ind current [W]ord" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
+		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume" })
+		vim.keymap.set("n", "<leader>f.", builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set(
+			"n",
+			"<leader>fe",
+			"<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>",
+			{ desc = "[F]ile [E]xplorer" }
+		)
+		vim.keymap.set("n", "<leader>fn", "<cmd>Telescope neoclip<CR>", { desc = "[F]ind in [N]eoclip" })
+		vim.keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "[F]ind [T]odo comments" })
+		-- Slightly advanced example of overriding default behavior and theme
+		vim.keymap.set("n", "<leader>/", function()
+			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				previewer = false,
+			}))
+		end, { desc = "[/] Fuzzily search in current buffer" })
 
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set("n", "<leader>s/", function()
-      builtin.live_grep({
-        grep_open_files = true,
-        prompt_title = "Live Grep in Open Files",
-      })
-    end, { desc = "[S]earch [/] in Open Files" })
-  end,
+		-- It's also possible to pass additional configuration options.
+		--  See `:help telescope.builtin.live_grep()` for information about particular keys
+		vim.keymap.set("n", "<leader>s/", function()
+			builtin.live_grep({
+				grep_open_files = true,
+				prompt_title = "Live Grep in Open Files",
+			})
+		end, { desc = "[S]earch [/] in Open Files" })
+	end,
 }
